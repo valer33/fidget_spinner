0b1f9b53a5172cf6324c41b7281289c83be9adbf
mpire <dockmpire@gmail.com>
2026-03-20 09:28:04 -0700

    remove

--- fidget_spinner/android/app/build.gradle.kts
+++ fidget_spinner/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.rushpocket.fidget_spinner"
+    namespace = "com.formfresh.fidget_spinner"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.rushpocket.fidget_spinner"
+        applicationId = "com.formfresh.fidget_spinner"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- fidget_spinner/android/app/src/main/kotlin/com/rushpocket/fidget_spinner/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.rushpocket.fidget_spinner
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
