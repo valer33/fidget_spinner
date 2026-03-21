8020e0e5dde1aff074286103d36d5dece03b4905
mpire <dockmpire@gmail.com>
2026-03-20 18:34:17 -0700

    Add physics, haptics, and extensible fidget framework

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,260 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FormFreshFidgetApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FormFreshFidgetApp extends StatelessWidget {
+  const FormFreshFidgetApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fidget Spinner v1',
+      title: 'FormFresh Fidgets',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.light,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const FidgetHomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+// Base class for all fidget types
+abstract class FidgetType {
+  String get name;
+  String get description;
+  Widget buildFidget(VoidCallback onInteract);
+  Future<void> onInteract();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class FidgetHomeScreen extends StatefulWidget {
+  const FidgetHomeScreen({Key? key}) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  State<FidgetHomeScreen> createState() => _FidgetHomeScreenState();
+}
+
+class _FidgetHomeScreenState extends State<FidgetHomeScreen> {
+  int _spinTime = 0;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      // Timer will be updated when actively spinning
     });
   }
 
   @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('FormFresh Fidgets'),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            // Fidget display area
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade100,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Center(
+                child: SpinnerFidget(
+                  onSpinStart: () {
+                    setState(() => _spinTime = 0);
+                  },
+                  onSpinEnd: (duration) {
+                    setState(() => _spinTime = duration);
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+
+            // Stats display
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.blue.shade50,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      const Text('Last Spin', style: TextStyle(fontSize: 12, color: Colors.grey)),
+                      Text(
+                        '$_spinTime s',
+                        style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text('Total Spins', style: TextStyle(fontSize: 12, color: Colors.grey)),
+                      const Text(
+                        '0',
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 24),
+
+            // Placeholder for future fidget types
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'More fidgets coming soon',
+              style: Theme.of(context).textTheme.bodyMedium,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class SpinnerFidget extends StatefulWidget {
+  final VoidCallback onSpinStart;
+  final Function(int) onSpinEnd;
+
+  const SpinnerFidget({
+    Key? key,
+    required this.onSpinStart,
+    required this.onSpinEnd,
+  }) : super(key: key);
+
+  @override
+  State<SpinnerFidget> createState() => _SpinnerFidgetState();
+}
+
+class _SpinnerFidgetState extends State<SpinnerFidget> with SingleTickerProviderStateMixin {
+  late AnimationController _rotationController;
+  double _currentVelocity = 0;
+  double _totalRotation = 0;
+  int _spinStartTime = 0;
+  Timer? _decayTimer;
+
+  static const double _friction = 0.95; // Deceleration factor
+  static const double _velocityThreshold = 0.01; // Minimum velocity to continue spinning
+  static const Duration _decayInterval = Duration(milliseconds: 50);
+
+  @override
+  void initState() {
+    super.initState();
+    _rotationController = AnimationController(vsync: this);
+  }
+
+  void _startSpin(double velocity) {
+    widget.onSpinStart();
+    _spinStartTime = DateTime.now().millisecondsSinceEpoch;
+    _currentVelocity = velocity;
+    _decayTimer?.cancel();
+
+    // Initial haptic feedback
+    HapticFeedback.heavyImpact();
+
+    _decayTimer = Timer.periodic(_decayInterval, (_) {
+      if (_currentVelocity.abs() < _velocityThreshold) {
+        _decayTimer?.cancel();
+        int spinDuration = (DateTime.now().millisecondsSinceEpoch - _spinStartTime) ~/ 1000;
+        widget.onSpinEnd(spinDuration);
+        _stopSpin();
+        return;
+      }
+
+      setState(() {
+        // Apply friction/deceleration
+        _currentVelocity *= _friction;
+        _totalRotation += _currentVelocity * 0.01;
+
+        // Haptic feedback pulses during spin (every 0.2 rotations)
+        if ((_totalRotation * 5).toInt() % 1 == 0) {
+          HapticFeedback.lightImpact();
+        }
+      });
+    });
+  }
+
+  void _stopSpin() {
+    _decayTimer?.cancel();
+    _currentVelocity = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _startSpin(8.0); // Initial spin velocity
+      },
+      child: Transform.rotate(
+        angle: _totalRotation * 2 * 3.14159, // Convert to radians
+        child: Container(
+          width: 150,
+          height: 150,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+              colors: [Colors.blue.shade400, Colors.purple.shade400],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.blue.shade200,
+                blurRadius: 12,
+                spreadRadius: 2,
+              ),
+            ],
+          ),
+          child: Center(
+            child: Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade300,
+                    blurRadius: 4,
+                    spreadRadius: 1,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _decayTimer?.cancel();
+    _rotationController.dispose();
+    super.dispose();
   }
 }
